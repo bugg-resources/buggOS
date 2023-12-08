6ccaaa2f6c1ab62e19402dbaca26bdd3fa9914a8
Jeff Gough <j@jeffreygough.com>
2023-12-08 12:43:14 +0000

    Add Bugg OS stage to manual build

--- build-setup.sh
+++ build-setup.sh
@@ -1,9 +1,9 @@
-git clone https://github.com/RPi-Distro/pi-gen.git
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install coreutils quilt parted qemu-user-static debootstrap zerofree zip \
-dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc \
-qemu-utils kpartx gpg pigz
+#git clone https://github.com/RPi-Distro/pi-gen.git
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
+#sudo apt-get -y install coreutils quilt parted qemu-user-static debootstrap zerofree zip \
+#dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc \
+#qemu-utils kpartx gpg pigz
 
 touch pi-gen/stage3/SKIP
 touch pi-gen/stage4/SKIP
@@ -11,5 +11,6 @@ touch pi-gen/stage5/SKIP
 touch pi-gen/stage4/SKIP_IMAGES
 touch pi-gen/stage5/SKIP_IMAGES
 cp config pi-gen/config
+cp -R stage-bugg-os pi-gen
 cd pi-gen
 #sudo ./build.sh
