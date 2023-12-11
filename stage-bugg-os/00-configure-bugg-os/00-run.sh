497a238c9210c764dcec55d6804a3263d0553858
Jeff Gough <j@jeffreygough.com>
2023-12-11 15:07:09 +0000

    Add debug print

--- stage-bugg-os/00-configure-bugg-os/00-run.sh
+++ stage-bugg-os/00-configure-bugg-os/00-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 # -e flag causes the script to exit if any command returns a non-zero exit code
-
+echo "---> Configuring Bugg OS"
+echo ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bees.txt
 echo "hello" > ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bees.txt
