d10ccca051a1bfb64343a368a02325b90a81fd33
Jeff Gough <j@jeffreygough.com>
2023-12-11 19:54:11 +0000

    Checkout Bugg userspace firmware and copy it into the user's home folder

--- stage-bugg-os/00-configure-bugg-os/00-run.sh
+++ stage-bugg-os/00-configure-bugg-os/00-run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
 # -e flag causes the script to exit if any command returns a non-zero exit code
-echo "---> Configuring Bugg OS"
+
+echo "---> Enable UART console"
+echo "enable_uart=1" >> ${ROOTFS_DIR}/boot/config.txt
+sed -i "s/ quiet//g" ${ROOTFS_DIR}/boot/cmdline.txt
+
+echo "---> Install Bugg Recording Userspace"
+# bugg-cm4-firmware has already been cloned by the workflow script
+install --owner=${FIRST_USER_NAME} --group=${FIRST_USER_NAME} bugg-cm4-firmware ${ROOTFS_DIR}/home/${FIRST_USER_NAME}
+
 echo ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bees.txt
 echo "hello" > ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bees.txt
