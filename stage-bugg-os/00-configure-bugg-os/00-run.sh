99701b6d0b04f2dc3a583e7236e126fbe3d0c421
Jeff Gough <j@jeffreygough.com>
2024-01-22 22:24:55 +0000

    Fix missing success print

--- stage-bugg-os/00-configure-bugg-os/00-run.sh
+++ stage-bugg-os/00-configure-bugg-os/00-run.sh
@@ -2,7 +2,7 @@
 # -e flag causes the script to exit if any command returns a non-zero exit code
 
 echo "Running stage-bugg-os/00-configure-bugg-os/00-run.sh"
-echo "  🔧 Configuring the Raspberry Pi"
+echo "  🔧 Configuring the Raspberry Pi Hardware"
 
 echo "    ⚙️ Enable UART console"
 echo "enable_uart=1" >> ${ROOTFS_DIR}/boot/firmware/config.txt
@@ -31,6 +31,7 @@ echo "watchdog-device=/dev/watchdog" >> ${ROOTFS_DIR}/etc/watchdog.conf
 echo "watchdog-timeout=15" >> ${ROOTFS_DIR}/etc/watchdog.conf
 echo "max-load-1 = 24" >> ${ROOTFS_DIR}/etc/watchdog.conf
 
+echo "  🍻 Success configuring hardware!"
 
 
 #echo ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bees.txt
