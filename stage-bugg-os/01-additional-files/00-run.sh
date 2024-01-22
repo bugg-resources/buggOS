167e6424151c11a45979224515b73da4a718af29
Jeff Gough <j@jeffreygough.com>
2024-01-22 21:57:23 +0000

    Fix typo

--- stage-bugg-os/01-additional-files/00-run.sh
+++ stage-bugg-os/01-additional-files/00-run.sh
@@ -9,4 +9,4 @@ echo "🔧 Installing additional files"
 # The first-boot.service systemd service will run this script once, on first boot.
 echo "    ⚙️ Install first-boot.service"
 install -m 644 first-boot.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 755 first-boot-script.sh "${ROOTFS_DIR}/usr/local/bin/"
+install -m 755 first-boot.sh "${ROOTFS_DIR}/usr/local/bin/"
