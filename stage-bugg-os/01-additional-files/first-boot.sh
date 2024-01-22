47fdf0a0d655faa8387aa547a98b1c1de58f73ef
Jeff Gough <j@jeffreygough.com>
2024-01-22 22:12:48 +0000

    Remove error checking - raspi-config is not consistent

--- stage-bugg-os/01-additional-files/first-boot.sh
+++ stage-bugg-os/01-additional-files/first-boot.sh
@@ -5,25 +5,27 @@
 echo "  🔧 Bugg-OS First Boot"
 
 echo "    ⚙️ Enable I2C"
-raspi-config nonint do_i2c 0 || echo "raspi-config: Error enabling I2C"
+raspi-config nonint do_i2c 0
 
 echo "    ⚙️ Enable Auto Login"
-raspi-config nonint do_boot_behaviour B2 || echo "raspi-config: Error enabling Auto Login"
+raspi-config nonint do_boot_behaviour B2
 
 echo "    ⚙️ Enable the serial port"
-raspi-config nonint do_serial_hw 0 || echo "raspi-config: Error enabling the serial port"
+raspi-config nonint do_serial_hw 0
 
 echo "    ⚙️ Enable remote GPIO access"
-raspi-config nonint do_rgpio 0 || echo "raspi-config: Error enabling remote GPIO access"
+raspi-config nonint do_rgpio 0
 
 echo "    ⚙️ Set Timezone to UTC"
-raspi-config nonint do_change_timezone UTC || echo "raspi-config: Error setting timezone" 
+raspi-config nonint do_change_timezone UTC
 
 echo "    ⚙️ Start the watchdog service"
-systemctl enable watchdog || echo "systemctl: Error enabling watchdog"
-systemctl start watchdog || echo "systemctl: Error starting watchdog"
-systemctl status watchdog  || echo "systemctl: Error checking watchdog status"
+systemctl enable watchdog
+systemctl start watchdog 
+systemctl status watchdog
 
 echo "    ⚙️ Disabling first-boot.service"
 systemctl stop first-boot.service
 systemctl disable first-boot.service
+
+echo "  🍻 Success!"
