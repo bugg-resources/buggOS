4fc6f0ae79bab7cd854c2ef267e5ef8ed941fc98
Ubuntu <ubuntu@ip-172-31-41-26.eu-north-1.compute.internal>
2024-01-22 21:29:28 +0000

    Fix go.sh script to copy in the bugg-os stage files each time

--- go.sh
+++ go.sh
@@ -1,5 +1,12 @@
+# This script can be run manually, as an alternative to running the build  in a CI pipeline
+
+# Copy current build files into /pi-gen
+cp config pi-gen/config
+cp -R stage-bugg-os pi-gen
+
+# Run the build
 cd pi-gen
-sudo ~/shellprof/shellprof ./build.sh
+/home/ubuntu/shellprof/shellprof ./build.sh
 cd -
 
 
