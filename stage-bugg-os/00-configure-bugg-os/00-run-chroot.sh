9567bed5040ca3dc347705e5f973ea31faf01bc2
Jeff Gough <j@jeffreygough.com>
2024-01-22 22:12:28 +0000

    Fuck it - the chroot thing doesn't work, at least not for raspi-config

--- stage-bugg-os/00-configure-bugg-os/00-run-chroot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-echo "  🔧 Running steps within chroot environment"
-echo "    ⚙️ Enable I2C"
-raspi-config nonint do_i2c 0 || echo "raspi-config: Error enabling I2C"
-echo "  🍻 Success!"
