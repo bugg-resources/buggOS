e9fc0936cae9d66988d4ca0c776fc0893e03559c
Jeff Gough <j@jeffreygough.com>
2024-01-23 09:53:25 +0000

    In first-boot,sh, use 'logger' calls to write to the system journal

--- stage-bugg-os/01-additional-files/first-boot.sh
+++ stage-bugg-os/01-additional-files/first-boot.sh
@@ -2,30 +2,33 @@
 
 # This script runs once, on first boot.
 # It is run by the first-boot.service systemd service.
-echo "  🔧 Bugg-OS First Boot"
+# Debug statements are logged to the journal.
+# To view the journal, run `journalctl -u first-boot.service -f`
 
-echo "    ⚙️ Enable I2C"
+logger -t first-boot.sh "🔧 Bugg-OS First Boot Script"
+
+logger "⚙️ Enable I2C"
 raspi-config nonint do_i2c 0
 
-echo "    ⚙️ Enable Auto Login"
+logger "⚙️ Enable Auto Login"
 raspi-config nonint do_boot_behaviour B2
 
-echo "    ⚙️ Enable the serial port"
+logger "⚙️ Enable the serial port"
 raspi-config nonint do_serial_hw 0
 
-echo "    ⚙️ Enable remote GPIO access"
+logger "⚙️ Enable remote GPIO access"
 raspi-config nonint do_rgpio 0
 
-echo "    ⚙️ Set Timezone to UTC"
+logger "⚙️ Set Timezone to UTC"
 raspi-config nonint do_change_timezone UTC
 
-echo "    ⚙️ Start the watchdog service"
+logger "⚙️ Start the watchdog service"
 systemctl enable watchdog
 systemctl start watchdog 
 systemctl status watchdog
 
-echo "    ⚙️ Disabling first-boot.service"
+logger "⚙️ Disabling first-boot.service"
 systemctl stop first-boot.service
 systemctl disable first-boot.service
 
-echo "  🍻 Success running first boot script!"
+logger -t first-boot.sh "🍻 First boot script complete."
