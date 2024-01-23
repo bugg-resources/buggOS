40c883e86a59461ba079573ca69db5d77b30d5ab
Ubuntu <ubuntu@ip-172-31-41-26.eu-north-1.compute.internal>
2024-01-23 15:22:57 +0000

    remove shellprof from go.sh

--- go.sh
+++ go.sh
@@ -1,12 +1,15 @@
 # This script can be run manually, as an alternative to running the build  in a CI pipeline
 
 # Copy current build files into /pi-gen
+rm pi-gen/config
+rm -rf pi-gen/stage-bugg-os
 cp config pi-gen/config
 cp -R stage-bugg-os pi-gen
 
 # Run the build
 cd pi-gen
-/home/ubuntu/shellprof/shellprof ./build.sh
+#/home/ubuntu/shellprof/shellprof ./build.sh
+./build.sh
 cd -
 
 
