3abf5ebfc6663c45e5f32f139fc9b9c72d62f569
Jeff Gough <j@jeffreygough.com>
2024-01-24 15:20:40 +0000

    Script for copying the built OS image out of the container to the host. Later this will be replaced with something more elegant.

---
+++ copy_img_to_host.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Get the ID of the first running container
+container_id=$(docker ps -q | head -n 1)
+
+# Check if a container ID was found
+if [ -z "$container_id" ]; then
+  echo "No running containers found."
+  exit 1
+fi
+
+# Source path inside the container
+file_pattern="/home/ubuntu/bugg-os/pi-gen/deploy/*.gz"
+matching_file=$(docker exec "$container_id" sh -c "ls $file_pattern")
+source_path=$matching_file
+
+# Destination path on the host
+destination_path="/home/ubuntu/deploy"
+
+# Copy the file from the container to the host
+docker cp "$container_id:$source_path" "$destination_path"
+
+# Check if the copy was successful
+if [ $? -eq 0 ]; then
+  echo "File copied successfully from container to host."
+else
+  echo "Failed to copy file from container to host."
+fi
