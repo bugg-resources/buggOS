e38ff41b017d450cd1a583fe7c4e0a551299d56f
Jeff Gough <j@jeffreygough.com>
2024-01-27 20:55:44 +0000

    RPi OS prevents installation of packages system-wide with PIP. Try with a venv for the bugg user, within the chroot

--- stage-bugg-os/02-python-dependencies/02-run.sh
+++ stage-bugg-os/02-python-dependencies/02-run.sh
@@ -6,9 +6,15 @@
 # Currently this takes a list of dependencies from the local requirements.txt file.
 # In the future, we should pull in the dependencies from the application firmware repo.
 
-on_chroot << EOF
-echo "  🔧 Installing Python dependencies"
+# RPi OS prevents installation of system-wide Python packages with PIP.
+# The recommended workflow is to create a virtual environment for the user, or use a venv for the project.
 
-echo "    ⚙️ Install Python dependencies"
+
+echo "  🔧 Installing Python dependencies"
+on_chroot << EOF
+VENV="${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.env"
+python3 -m venv --system-site-packages "$VENV"
+source "$VENV/bin/activate"
+
 pip3 install -r requirements.txt
 EOF
