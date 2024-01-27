88e660d99d9433b6aee48805639f815b14609c89
Jeff Gough <j@jeffreygough.com>
2024-01-27 19:24:11 +0000

    Add comment explaining the origin of the bugg-recording repo

--- stage-bugg-os/03-application-firmware/03-run.sh
+++ stage-bugg-os/03-application-firmware/03-run.sh
@@ -2,6 +2,7 @@
 # -e flag causes the script to exit if any command returns a non-zero exit code
 
 # This script installs the Python dependencies for the application software.
+# The bugg-recording repo is cloned into files/bugg-recording by the GA workflow.
 
 mkdir -p "${ROOTFS_DIR}/opt"
 cp -r "${STAGE_DIR}/files/bugg-recording" "${ROOTFS_DIR}/opt/"
