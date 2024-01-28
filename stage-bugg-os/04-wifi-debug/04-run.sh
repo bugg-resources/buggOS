072eaff2f3ec36ed47f2ed671da8d2ad28c30c56
Jeff Gough <j@jeffreygough.com>
2024-01-28 14:59:21 +0000

    Try to enable wifi via the pi-gen config file

--- stage-bugg-os/04-wifi-debug/04-run.sh
+++ stage-bugg-os/04-wifi-debug/04-run.sh
@@ -9,7 +9,7 @@
 # RPi OS prevents installation of system-wide Python packages with PIP.
 # The recommended workflow is to create a virtual environment for the user, or use a venv for the project.
 
-echo "  🔧 Configuring Wifi for Debugging"
-on_chroot << EOF
-SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_wifi_ssid_passphrase "The Church Workshop 2.4 GHz" "fromaltartoworkbench"
-EOF
+# echo "  🔧 Configuring Wifi for Debugging"
+# on_chroot << EOF
+# SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_wifi_ssid_passphrase "The Church Workshop 2.4 GHz" "fromaltartoworkbench"
+# EOF
