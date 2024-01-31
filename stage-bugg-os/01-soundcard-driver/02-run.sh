45fd82bd59e9b76c8190b025ee7bf77e59b912c0
Jeff Gough <j@jeffreygough.com>
2024-01-31 19:31:42 +0000

    Add step to install sound driver

---
+++ stage-bugg-os/01-soundcard-driver/02-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+# -e flag causes the script to exit if any command returns a non-zero exit code
+
+# This script builds and installs the Bugg soundcard driver kernel module.
+
+
+echo "🔧 Installing Bugg soundcard driver"
+mkdir -p "${ROOTFS_DIR}/opt"
+cp -r "${STAGE_DIR}/files/bugg-soundcard-driver" "${ROOTFS_DIR}/opt/"
+
+on_chroot << EOF
+cd /opt/bugg-soundcard-driver/codec
+make
+make install
+
+cd /opt/bugg-soundcard-driver/dt
+make
+make install
+EOF
