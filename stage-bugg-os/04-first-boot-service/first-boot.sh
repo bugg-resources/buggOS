7bb3dbbde2a158b90b12f66bc807667c09bc1511
Jeff Gough <j@jeffreygough.com>
2024-03-15 23:21:48 +0000

    Disable the first-boot service and enable the bugg-test service once the system is fully configured

--- stage-bugg-os/04-first-boot-service/first-boot.sh
+++ stage-bugg-os/04-first-boot-service/first-boot.sh
@@ -6,10 +6,19 @@
 # To view the journal, run `journalctl -u first-boot.service -f`
 
 
-RAN_FLAG="/opt/.first-boot.sh-ran"   
-
 logger -t first-boot.sh "🔧 Bugg-OS First Boot Script"
 
+RAN_FLAG="/opt/.first-boot.sh-ran"   
 echo "Ran at `date`" >> $RAN_FLAG
 
-logger -t first-boot.sh "🍻 First boot script complete."
+# By the time this script runs, the root filesystem has been expanded to fill the SD card,
+# the system had rebooted once, and the first-boot.service has run, installing the Bugg soundcard driver.
+# Now we enable the testing service, disable the first-boot service, and reboot the system so the soundcard driver can load.
+logger -t first-boot.sh "⚙️ Enabling the bugg-test service"
+systemctl enable bugg-test.service
+
+loggger -t first-boot.sh "⚙️ Disabling first-boot.service"
+systemctl disable first-boot.service
+
+logger -t first-boot.sh "🍻 First boot script complete - rebooting the system"
+reboot
