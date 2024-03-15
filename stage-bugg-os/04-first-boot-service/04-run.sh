1bcd27640c7a36192a72a39012f9507eab63e3a5
Jeff Gough <j@jeffreygough.com>
2024-03-15 20:15:24 +0000

    Fix symlink

--- stage-bugg-os/04-first-boot-service/04-run.sh
+++ stage-bugg-os/04-first-boot-service/04-run.sh
@@ -11,6 +11,6 @@ echo "⚙️ Install first-boot.service"
 install -m 644 first-boot.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 755 first-boot.sh "${ROOTFS_DIR}/usr/local/bin/"
 mkdir -p "${ROOTFS_DIR}/etc/systemd/system/first-boot-complete.target.wants"
-ln -s "${ROOTFS_DIR}/etc/systemd/system/first-boot.service" "${ROOTFS_DIR}/etc/systemd/system/first-boot-complete.target.wants/first-boot.service"
+ln -s "${ROOTFS_DIR}/etc/systemd/system/first-boot.service" "/etc/systemd/system/first-boot-complete.target.wants/first-boot.service"
 
 echo "🍻 Success installing first boot service!"
