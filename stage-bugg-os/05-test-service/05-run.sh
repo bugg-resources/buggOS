38b58b9f6382e0d91bcc400f792258926e30c51d
Jeff Gough <j@jeffreygough.com>
2024-03-15 23:23:23 +0000

    systemd unit that provides a hardware test service when the hardware is fully configured, soundcard driver installed, etc

---
+++ stage-bugg-os/05-test-service/05-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+# -e flag causes the script to exit if any command returns a non-zero exit code
+
+# This script is used to install additional files into the rootfs.
+
+echo "🔧 Installing testing service"
+
+
+# The first-boot.service enables this testing service once the system is fully configured
+echo "⚙️ Install bugg-test.service"
+install -m 644 bugg-test.service "${ROOTFS_DIR}/etc/systemd/system/"
+
+echo "🍻 Success installing first boot service!"
