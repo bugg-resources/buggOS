1b56feec7bf9b2b531e00ad50ed3a0342fa14b91
Ubuntu <ubuntu@ip-172-31-41-26.eu-north-1.compute.internal>
2024-04-02 03:25:17 +0000

    Enable artefact server

--- build_normal.sh
+++ build_normal.sh
@@ -1,5 +1,6 @@
 git pull \
 && sudo find deploy/ -type f -delete;\
 time sudo /home/ubuntu/bin/act -j image-builder  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` -b /home/ubuntu/bugg-os/deploy:/home/ubuntu/bugg-os/pi-gen/deploy \
+--artifact-server-path /home/ubuntu/bugg-os/artifact \
 && sudo ./copy_img_to_host.sh
 
