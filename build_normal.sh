892b4fadaa3831f6ea6e71272e19c372782775d6
Ubuntu <ubuntu@ip-172-31-41-26.eu-north-1.compute.internal>
2024-04-02 00:57:10 +0000

    Bind the deploy dir in the container into the /deploy folder in the repo. Not sure whether this is working.

--- build_normal.sh
+++ build_normal.sh
@@ -1,5 +1,5 @@
 git pull \
 && sudo find deploy/ -type f -delete;\
-time sudo /home/ubuntu/bin/act -j image-builder  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
+time sudo /home/ubuntu/bin/act -j image-builder  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` -b /home/ubuntu/bugg-os/deploy:/home/ubuntu/bugg-os/pi-gen/deploy \
 && sudo ./copy_img_to_host.sh
 
