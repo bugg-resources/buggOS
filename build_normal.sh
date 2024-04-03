2f8b04a313693fbfb23033e5eaa592fdecee7e62
Jeff Gough <j@jeffreygough.com>
2024-04-03 19:07:09 +0100

    Move option to command line, actrc is a bitch

--- build_normal.sh
+++ build_normal.sh
@@ -1,4 +1,3 @@
 git pull \
 && sudo find deploy/ -type f -delete;\
-time sudo /home/ubuntu/bin/act --bind -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
-
+time sudo /home/ubuntu/bin/act --bind --artifact-server-path /tmp/artifacts -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
