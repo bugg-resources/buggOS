60c58c922757f3c03b1bf048812a94b181057811
Ubuntu <ubuntu@ip-172-31-41-26.eu-north-1.compute.internal>
2024-04-03 12:46:37 +0000

    Update name of build job in launch scripts

--- build_normal.sh
+++ build_normal.sh
@@ -1,6 +1,6 @@
 git pull \
 && sudo find deploy/ -type f -delete;\
-time sudo /home/ubuntu/bin/act -j image-builder  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` -b /home/ubuntu/buggOS/deploy:/home/ubuntu/buggOS/pi-gen/deploy \
+time sudo /home/ubuntu/bin/act -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` -b /home/ubuntu/buggOS/deploy:/home/ubuntu/buggOS/pi-gen/deploy \
 --artifact-server-path /home/ubuntu/buggOS/artifact \
 && sudo ./copy_img_to_host.sh
 
