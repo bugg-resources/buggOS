0c5e9871ef96a38382e95b120280c9338768ca33
Jeff Gough <j@jeffreygough.com>
2024-04-03 18:48:38 +0100

    Add script to fetch artefact from AWS runner

---
+++ fetch_and_flash.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+url=$1
+
+if [ -z "$url" ]; then
+  echo "Usage: $0 <url>"
+  exit 1
+fi
+
+KEY_FILE="/home/jeff/tools/soft/amazon-aws/aws-keys.pem"
+BUGG="/dev/sda"
+
+if [ ! -e "$BUGG" ]; then
+  echo "CM4 device not found - make sure rpiboot is running"
+  exit 1
+fi 
+
+if [[ $url != http* ]]; then
+  if [ -f "$KEY_FILE" ]; then
+      KEY="--key $KEY_FILE"
+  else
+    echo "Not an HTTP URL and no key found at ${KEY_FILE}. Exiting."  
+    exit 1
+  fi
+fi
+
+TADA="fish -c tada"
+
+time curl \
+  $KEY \
+  $url \
+  | gunzip \
+  | sudo dd bs=10M of=BUGG \
+  && $TADA
