f3c360ded8e84da36649e5e706e2bb7d8e593d88
Ubuntu <ubuntu@ip-172-31-41-26.eu-north-1.compute.internal>
2024-04-03 15:17:01 +0000

    Now we're using act in --bind mode, there's no need for a copy-out-of-container system

--- build_normal.sh
+++ build_normal.sh
@@ -1,5 +1,4 @@
 git pull \
 && sudo find deploy/ -type f -delete;\
 time sudo /home/ubuntu/bin/act --bind -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
-&& sudo ./copy_img_to_host.sh
 
