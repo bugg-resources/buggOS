c2506efa28cb587355272fb39f3b737780b6014d
Ubuntu <ubuntu@ip-172-31-41-26.eu-north-1.compute.internal>
2024-04-03 13:54:36 +0000

    De-cruft the build launch scripts

---
+++ build_keep_runner_alive.sh
@@ -0,0 +1,5 @@
+git pull \
+&& sudo find deploy/ -type f -delete;\
+time sudo /home/ubuntu/bin/act --reuse -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
+&& sudo ./copy_img_to_host.sh
+
--- build_normal.sh
+++ build_normal.sh
@@ -1,5 +1,5 @@
 git pull \
 && sudo find deploy/ -type f -delete;\
-time sudo /home/ubuntu/bin/act --reuse -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
+time sudo /home/ubuntu/bin/act -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
 && sudo ./copy_img_to_host.sh
 
