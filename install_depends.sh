cf979c4b80a6a79b5433452411878e9a407d29c2
Jeff Gough <j@jeffreygough.com>
2024-04-03 14:16:07 +0100

    Only run apt-install once

--- install_depends.sh
+++ install_depends.sh
@@ -6,18 +6,23 @@
 # installs the dependencies accordingly. apt-get on its own doesn't support
 # this kind of dependency resolution.
 
+# Function to install dependencies
 install_dependencies() {
-    # Read the dependency file and install dependencies
+    # Declare an array to hold package names
+    packages=()
+
+    # Read the dependency file and populate the array
     while IFS=: read -r package dependency; do
-        # If there's a colon in the line
+        # If there's a colon in the line, add dependency to packages array
         if [[ -n $dependency ]]; then
-            echo "Installing $dependency"
-            sudo apt-get install -y "$dependency"
+            packages+=("$dependency")
         else
-            echo "Installing $package"
-            sudo apt-get install -y "$package"
+            packages+=("$package")
         fi
     done < "$1"
+
+    # Install all packages using apt-get install
+    sudo apt-get install -y "${packages[@]}"
 }
 
 # Usage: install_dependencies <dependency_file>
