c0017dfa3a7715bb7c65828ea0e82867fd9c03b6
Jeff Gough <j@jeffreygough.com>
2024-04-03 19:20:33 +0100

    Include release job

--- build_normal.sh
+++ build_normal.sh
@@ -1,3 +1,6 @@
 git pull \
 && sudo find deploy/ -type f -delete;\
-time sudo /home/ubuntu/bin/act --bind --artifact-server-path /tmp/artifacts -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
+time sudo /home/ubuntu/bin/act --bind \
+--artifact-server-path /tmp/artifacts \
+-j release \
+-s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
