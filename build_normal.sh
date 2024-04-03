b21be3dcc681791ffdc945606bc46cb0f9015433
Ubuntu <ubuntu@ip-172-31-41-26.eu-north-1.compute.internal>
2024-04-03 17:56:26 +0000

    Remove -P

--- build_normal.sh
+++ build_normal.sh
@@ -1,4 +1,4 @@
 git pull \
 && sudo find deploy/ -type f -delete;\
-time sudo /home/ubuntu/bin/act --bind -P -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
+time sudo /home/ubuntu/bin/act --bind -j build  -s GITHUB_TOKEN=`cat /home/ubuntu/github_token` \
 
